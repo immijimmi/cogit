5eab76ffde199924a6ac95f27787e240835cada4
immijimmi <immijimmi1@gmail.com>
2026-06-30 03:07:26 +0100

    Added files for dev nginx config, and edited startup scripts

--- tools/start-all-dev.bat
+++ tools/start-all-dev.bat
@@ -1,7 +1,23 @@
 @echo off
-cd ..
+:: Necessary to ensure loop variables are expanded during loop execution rather than during parsing
+setlocal enabledelayedexpansion
+
+:: Read variables from the .env file (should include NGINX_PATH)
+for /f "tokens=1,* delims==" %%A in (.env) do (
+    set "linestart=%%A"
+    :: Ignore blank lines and lines starting with '#' (comments)
+    if not "!linestart!"=="" if not "!linestart!:~0,1!"=="#" (
+        set "%%A=%%B"
+    )
+)
+
+cd nginx/dev
+mkdir logs
+start "" "%NGINX_PATH%" -c nginx.conf
+
+cd ../../..
 
 start cmd /c "@echo off && cd server && npm start"
 start cmd /c "@echo off && cd client && npm start"
 
-ngrok http 3000
+ngrok http 8080
