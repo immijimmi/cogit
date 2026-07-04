661d35090a980eb693d79f5bccc5e7a006a8be56
immijimmi <immijimmi1@gmail.com>
2026-07-04 19:49:28 +0100

    Backend optimisation

--- server/src/index.js
+++ server/src/index.js
@@ -4,20 +4,36 @@ import fs from "node:fs/promises";
 import { PORT } from "./constants.js";
 import RequestLogger from "./cls/requestLogger.js";
 
+const METADATA_READ_COOLDOWN_MS = 1000 * 30; // 30 seconds
+
 const app = express();
 app.use(express.json());
 // CORS is globally allowed - this server does not need to discriminate based on request origin
 app.use(cors());
 
-// Request handlers
+let metadata = null;
+let lastMetadataReadMs = null;
 app.get("/api/metadata", async (req, res) => {
-  try {
-    const bytes = await fs.readFile("../metadata.json", "utf8");
-    const data = JSON.parse(bytes);
+  const nowMs = Date.now();
+  const isReadRecently =
+    lastMetadataReadMs !== null &&
+    nowMs - lastMetadataReadMs < METADATA_READ_COOLDOWN_MS;
 
-    res.json({ ok: true, data: data });
-  } catch (err) {
-    res.status(500).json("Unable to retrieve server metadata");
+  if (isReadRecently) {
+    return res.json({ ok: true, data: metadata });
+  } else {
+    try {
+      const bytes = await fs.readFile("../metadata.json", "utf8");
+      metadata = JSON.parse(bytes);
+      lastMetadataReadMs = nowMs;
+
+      return res.json({ ok: true, data: metadata });
+    } catch (err) {
+      return res.status(500).json({
+        error: "Internal Server Error",
+        message: "Unable to retrieve server metadata",
+      });
+    }
   }
 });
 
